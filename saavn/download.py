7886110f7f15f8361a85279200a0aaef9c9112a5
Maanuj <maanujvora@gmail.com>
2021-07-09 23:48:45 -0700

    Saavn Song Downloading Works

--- saavn/download.py
+++ saavn/download.py
@@ -3,7 +3,8 @@ import json
 from mutagen.id3 import ID3NoHeaderError
 from mutagen.id3 import ID3, TIT2, TALB, TPE1, TPE2, COMM, TCOM, TCON, TDRC, APIC
 from pydub import AudioSegment
-
+from moviepy.editor import *
+import taglib
 
 def content(name, data):
     r=requests.get(data.get('media_url'))
@@ -13,22 +14,18 @@ def content(name, data):
             f.write(chunk)
     f.close()
 
-    fileName = data.get('song').replace(' ', '_') + data.get('id') + ".mp3"
+    fileName = "songs/" + data.get('song').replace(' ', '_') + data.get('id') + ".mp3"
 
-    mp4_version = AudioSegment.from_file(name, "mp4")
-    mp4_version.export(fileName, format="mp3")
+    mp4 = r"song.mp4"
 
-    try: 
-        tags = ID3(fileName)
-    except ID3NoHeaderError:
-        tags = ID3()
+    audio = AudioFileClip(mp4)
+    audio.write_audiofile(fileName)
+    audio.close()
 
-    tags["TIT2"] = TIT2(encoding=3, text=data.get('song'))
-    tags["TALB"] = TALB(encoding=3, text=data.get('album'))
-    tags["TPE1"] = TPE1(encoding=3, text=data.get('singers'))
-    tags["TCOM"] = TCOM(encoding=3, text=data.get('music'))
-
-    tags.save(fileName)
+    song = taglib.File(fileName)
+    song.tags['TITLE'] = [data.get('song')]
+    song.tags["ALBUM"] = [data.get('album')]
+    song.save()
 
 def downloadSong(name, data):
     name=name+".mp4"
